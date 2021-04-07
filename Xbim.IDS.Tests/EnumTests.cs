76cb3d210df0a34762c27962c9222fdbb1e2293e
Claudio Benghi <claudio.benghi@gmail.com>
2021-04-07 23:58:59 +0200

    FacetBase Location changed.

---
+++ Xbim.IDS.Tests/EnumTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xbim.IDS.Tests
+{
+	[TestClass]
+	public class EnumTests
+	{
+		[TestMethod]
+		public void CanSetEnum()
+		{
+			Xids.IfcClassificationFacet f = new Xids.IfcClassificationFacet();
+
+			var values = Enum.GetValues(typeof(Xids.Location)).Cast<Xbim.Xids.Location>();
+			foreach (var val in values)
+			{
+				f.SetLocation(val);
+				Assert.AreEqual(val, f.GetLocation());
+			}
+		}
+	}
+}
--- Xbim.Xids/Facets/FacetBase.cs
+++ Xbim.Xids/Facets/FacetBase.cs
@@ -4,11 +4,12 @@ using System.Text;
 
 namespace Xbim.Xids
 {
+	[Flags]
 	public enum Location
 	{
-		any,
-		instance,
-		type,
+		any = 3,
+		instance = 1,
+		type = 2,
 	}
 
 	public enum Use
@@ -21,7 +22,37 @@ namespace Xbim.Xids
 
 	public abstract class FacetBase : IEquatable<FacetBase>
 	{
-		public string Location { get; set; } = null; // attribute location
+		private string location = Xbim.Xids.Location.any.ToString();
+
+		public Location GetLocation()
+		{
+			if (Enum.TryParse<Location>(location, out var loc))
+			{
+				return loc;
+			}
+			return Xbim.Xids.Location.any;
+		}
+		public void SetLocation(Location loc)
+		{
+			location = loc.ToString();
+		}
+
+
+
+		/// <summary>
+		/// String value of location, use <see cref="GetLocation()"/> for the enum.
+		/// Setting an invalid string will ignore the change.
+		/// </summary>
+		public string Location
+		{
+			get => location;
+			set
+			{
+				if (Enum.TryParse<Location>(location, out _))
+					location = value;
+			}
+		}
+
 		public string Uri { get; set; } = null; // attribute href
 		public string Use { get; set; } = null; // attribute use
 		public string Instructions { get; set; } // element
